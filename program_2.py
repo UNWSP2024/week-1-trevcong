117282f37144ac8da6bccf67d13e36c0430b126e
Trevor Conger <149925363+trevcong@users.noreply.github.com>
2024-08-28 18:57:25 -0500

    This program prints (Proverbs 22:29) in 3 lines!

--- program_2.py
+++ program_2.py
@@ -1,6 +1,14 @@
+# AUTHOR: Trevor Conger UNWSP
+# DATE: 8/28/24
+# TITLE: Proverbs 22:29 in 3 lines
+
+
+
+
+# This function prints (Proverbs 22:29) in 3 lines
 def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
+    print('“Do you see a man skillful in his work? ')
+    print('He will stand before kings;')
+    print('he will not stand before obscure men.”')
 
 proverbs_22_29()
