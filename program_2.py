01e7162aea45c3493a56a4f28b8ca57097bb371f
Trevor Conger <149925363+trevcong@users.noreply.github.com>
2024-09-04 23:08:12 -0500

    Changed to work?

--- program_2.py
+++ program_2.py
@@ -10,5 +10,6 @@ def proverbs_22_29():
     print('“Do you see a man skillful in his work? ')
     print('He will stand before kings;')
     print('he will not stand before obscure men.”')
+    print('    (Proverbs 22:29)')
 
 proverbs_22_29()
